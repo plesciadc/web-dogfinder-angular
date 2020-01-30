dbf5791ff2a2bf507fd08a8fce8a1d213a659de4
Daniel Plescia <plesciadc@me.com>
2020-01-30 11:20:24 -0500

    Add Spring Boot backend

---
+++ back-end-code/src/test/java/com/danielplescia/dogfinder/DogfinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danielplescia.dogfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogfinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
