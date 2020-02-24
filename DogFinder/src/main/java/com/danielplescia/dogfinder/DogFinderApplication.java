dd1da31090a29a164a0cdb1615c5880f5eb5e736
Daniel Plescia <plesciadc@me.com>
2020-02-23 21:19:19 -0500

    Spring boot app added

---
+++ DogFinder/src/main/java/com/danielplescia/dogfinder/DogFinderApplication.java
@@ -0,0 +1,13 @@
+package com.danielplescia.dogfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogFinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DogFinderApplication.class, args);
+    }
+
+}
